e0f75c1d1da14ea1a7fa429cc1262e839c037ab1
曾偉珉 <ryan10328@hotmail.com>
2016-03-06 13:38:54 +0800

    [Add] Delete Product Data

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -13,17 +13,21 @@ namespace MVC5Course.Controllers
         FabricsDbConext db = new FabricsDbConext();
         public ActionResult Index()
         {
-            var product = new Product()
+            //var product = new Product()
+            //{
+            //    ProductName = "Beer",
+            //    Price = 2,
+            //    Stock = 1,
+            //    Active = true
+            //};
+
+            //db.Products.Add(product);
+            var products = db.Products.OrderByDescending(x => x.ProductId).Take(5);
+
+            foreach (var item in products)
             {
-                ProductName = "Beer",
-                Price = 2,
-                Stock = 1,
-                Active = true
-            };
-
-            db.Products.Add(product);
-
-          
+                item.Price = item.Price + 1;
+            }
 
             try
             {
@@ -41,18 +45,46 @@ namespace MVC5Course.Controllers
                     }
                 }
             }
-            var singleProduct = db.Products.Where(x => x.ProductId == product.ProductId).ToList();
-            return View(singleProduct);
+            // var singleProduct = db.Products.Where(x => x.ProductId == product.ProductId).ToList();
+            return View(db.Products.OrderByDescending(x => x.ProductId).Take(5));
         }
 
         public ActionResult Details(int? id)
         {
-            if(!id.HasValue)
+            if (!id.HasValue)
             {
                 return View();
             }
             var product = db.Products.Find(id);
             return View(product);
         }
+
+        public ActionResult Delete(int? id)
+        {
+            var product = db.Products.Find(id);
+
+            //1. 土法煉鋼版
+            //var ol = db.OrderLines.Where(x => x.ProductId == product.ProductId);
+            //foreach(var item in ol)
+            //{
+            //    db.OrderLines.Remove(item);
+            //}
+
+            // 2. 稍微改進版
+            //foreach (var item in product.OrderLines)
+            //{
+            //    db.OrderLines.Remove(item);
+            //}
+
+            // 3. 一行幹掉版
+            // 有FK所以要先刪除子表資料
+            db.OrderLines.RemoveRange(product.OrderLines);
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
