5b91a936019ddd281af5c0eb91664cb24e5b5931
曾偉珉 <ryan10328@hotmail.com>
2016-03-06 10:32:29 +0800

    [Add] Add EF Controller
    1. Add EFController
    2. Add EFController Index View

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,30 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new FabricsDbConext();
+
+            db.Products.Add(new Product()
+            {
+                ProductName = "Water",
+                Price = 3,
+                Stock = 1,
+                Active = true
+            });
+
+            db.SaveChanges();
+
+            var data = db.Products.ToList();
+            return View(data);
+        }
+    }
+}
