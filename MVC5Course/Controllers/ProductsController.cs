762de71570d4d0f065c69b672d10fcd57b144a3f
曾偉珉 <ryan10328@hotmail.com>
2016-03-12 13:29:29 +0800

    建立BaseController並覆寫HandleUnknowAction方法

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -10,7 +10,7 @@ using MVC5Course.Models;
 
 namespace MVC5Course.Controllers
 {
-    public class ProductsController : Controller
+    public class ProductsController : BaseController
     {
         ProductRepository repo = RepositoryHelper.GetProductRepository();
         // GET: Products
