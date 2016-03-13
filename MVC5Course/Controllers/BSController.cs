b8babc1190b97f50a0ad5eb55a3258a5245713f3
曾偉珉 <ryan10328@hotmail.com>
2016-03-13 15:00:29 +0800

    Layout Practice

---
+++ MVC5Course/Controllers/BSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class BSController : Controller
+    {
+        // GET: BS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
