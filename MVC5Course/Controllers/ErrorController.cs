67bcd4efe39726df8ffd236af16a8598053d0a38
曾偉珉 <ryan10328@hotmail.com>
2016-03-13 11:42:33 +0800

    HandleErrorAttribute

---
+++ MVC5Course/Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HandleError(View = "Error2", ExceptionType = typeof(FormatException))]
+        public ActionResult Index(string e)
+        {
+            if (string.IsNullOrEmpty(e))
+            {
+                return View();
+            }
+            else
+            {
+                if (e == "1")
+                {
+                    throw new FormatException("格式錯誤");
+                }
+                if (e == "2")
+                {
+                    throw new ArgumentException("參數錯誤");
+                }
+            }
+         
+            return View();
+        }
+    }
+}
