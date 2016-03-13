073f078350ea9d80e879d87c877a798c9f855f04
曾偉珉 <ryan10328@hotmail.com>
2016-03-13 16:42:02 +0800

    PartialView vs Action

---
+++ MVC5Course/Controllers/PVController.cs
@@ -0,0 +1,36 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class PVController : Controller
+    {
+        ProductRepository repo = RepositoryHelper.GetProductRepository();
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Product product = db.Products.Find(id);
+            Product product = repo.Get(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        public ActionResult OrderLines(int id)
+        {
+            var orderlines = repo.Get(id).OrderLines;
+            return View(orderlines);
+        }
+    }
+}
